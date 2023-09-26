c665690b4bafed756c7cb4d0047da3614342b75c
kavya-adusumilli <143990653+kavya-adusumilli@users.noreply.github.com>
2023-09-26 10:34:44 +0530

    Create SimpleAddition.java

---
+++ SimpleAddition.java
@@ -0,0 +1,16 @@
+//java program to add two numbers
+//23.08.23
+//SimpleAddition.java
+//kavya
+import java.io.*;
+import java.util.*;
+public class SimpleAddition
+{
+	public static void main(String args[])
+  {
+	int x=10;
+	int y=12;
+	int z=x+y;
+	System.out.println("sum of x+y="+z);
+  }
+}
