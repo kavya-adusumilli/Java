ffc7d1298853bbfd34461060fbbda1ecf706f1b7
kavya-adusumilli <143990653+kavya-adusumilli@users.noreply.github.com>
2023-09-26 10:39:07 +0530

    Create IfExample.java

---
+++ IfExample.java
@@ -0,0 +1,16 @@
+//java program to demonstrate simple if
+//23.08.23
+//IfExample.java
+//kavya
+public class IfExample
+{
+	public static void main(String args[])
+  {
+	int x=10;
+	int y=12;
+	if(x+y>20)
+   {
+	System.out.println("x+y is greater than 20");
+   }
+  }
+}
