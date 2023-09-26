376bfa1972c1501caf5cd28cce27c7c75a3d495d
kavya-adusumilli <143990653+kavya-adusumilli@users.noreply.github.com>
2023-09-26 10:37:01 +0530

    Create MethodOverloadingDemo1.java

---
+++ MethodOverloadingDemo1.java
@@ -0,0 +1,23 @@
+//java program to demonstrate method overloading
+//13.09.23
+//kavya
+import java.io.*;
+class Adder {
+	int add(int a,int b) {
+		return a+b;
+	}
+	int add(int a,int b,int c) {
+		return a+b+c;
+	}
+}
+class MethodOverloadingDemo1 {
+	public static void main(String args[]) {
+		Adder ad=new Adder();
+		int x=ad.add(5,10);
+		int y=ad.add(10,20,30);
+		int z=ad.add(x,y);
+		System.out.println("x value is "+x);
+		System.out.println("y value is "+y);
+		System.out.println("z value is "+z);
+	}
+}
