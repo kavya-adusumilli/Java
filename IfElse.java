a2900e55d39e83210dab4e7d9b271f569ea109ab
kavya-adusumilli <143990653+kavya-adusumilli@users.noreply.github.com>
2023-09-26 10:40:04 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,20 @@
+//java program to demonstrate if else
+//23.08.23
+//IfElse.java
+//kavya
+public class IfElse
+{
+	public static void main(String args[])
+  {
+	int x=10;
+	int y=8;
+	if (x+y>20)
+         {
+	   System.out.println("x+y is greater than 20");
+         }
+          else
+           {
+	     System.out.println("x+y is less than 20");
+           }
+   }
+}
