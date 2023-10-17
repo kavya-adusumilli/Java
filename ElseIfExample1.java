7523bb91eda1c0524a16ba2450382c88a3655e32
kavya-adusumilli <143990653+kavya-adusumilli@users.noreply.github.com>
2023-10-17 09:45:36 +0530

    Update ElseIfExample1.java

--- ElseIfExample1.java
+++ ElseIfExample1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
-public class ElseIfExample1 {
-    public static void main(String args[]) {
+public class ElseIfExample{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter marks of 3 subjects");
         float s1=sc.nextFloat();
@@ -12,7 +12,20 @@ public class ElseIfExample1 {
         System.out.println("The total marks of student is"+total);
         if(avg>=75)
         {
+            if(avg>=95)
+            {
+                System.out.println("student avg is"+avg+"your avg is top5");
+            }
+                else if(avg>=90)
+                {
+       
+                System.out.println("student avg is"+avg+"your avg is top10");
+                   
+                }
+            else
+            {
             System.out.println("student avg is"+avg+"student passed with distinction");
+            }
         }
         else if(avg>=60&&avg<75)
         {
